241846051d25d128b6ccca1da90972a47dc44405
Kevin <khomer8@yahoo.com>
2014-05-26 15:08:30 -0500

    JSON to hold all the information
    -Instead of making variables in the javascript of strings or passing
    huge strings of information in the html file. I created a JSONObject to
    hold all the information of each example
    -TitleOfExample.src/code/info are the three attributes the object has.
    So instead a huge string for the src of a exmaple I use:
    JSONObject.TitleOfExample.src for the argument in the loadiframe()

---
+++ json.js
@@ -0,0 +1,11 @@
+var JSONObject = {
+	"snow":{
+		"src":"http://www.whaleblubber.net/Kevin/html/snow/SnowFall.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/SnowFall",
+		"info":"Snow is one part of a weather collection I am creating.  My goal is to create different weather types in javascript",
+	},
+	"leaf":{
+		"src":"http://www.whaleblubber.net/Kevin/html/leaf/LeafFall.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/LeafFall" 
+	}
+};
