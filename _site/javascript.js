bf1583b688133469da47813561915b4bc2c68c2f
Kevin Bacon <khomer8@yahoo.com>
2014-05-31 22:37:12 -0500

    jekyll installed?
    don't know how to use it kk

---
+++ _site/javascript.js
@@ -0,0 +1,64 @@
+function mouseoverexamples(event){
+  var li = document.getElementsByTagName('li');
+  for (var i = 0; i < li.length; i++) {
+    li[i].addEventListener("mouseover", function( event ) {   
+      event.target.style.fontSize = 110 + "%";
+      event.target.style.textDecoration = "underline";
+      event.target.style.listStyleType = 'square';
+      document.body.style.cursor = 'pointer';
+    }, false);
+    li[i].addEventListener("mouseout", function( event ) {
+      event.target.style.fontSize = 100 + "%";
+      event.target.style.textDecoration = "none";
+      event.target.style.listStyleType = 'none';
+      document.body.style.cursor = 'default';
+    },false);
+  };
+}
+// Is the windows application still broken?
+function LoadiFrame(link,source,info,controls){
+  var frame = document.getElementById("frame");
+  var sourcecode = document.getElementById("sourcecodelink");
+  var clearbutton = document.getElementById("clear");
+  var sourcebutton = document.getElementById("sourcecode");
+  var exampleinfo = document.getElementById("exampleinfo");
+  var examplecontrols = document.getElementById("examplecontrols");
+
+  if(exampleinfo.innerHTML === undefined){
+    exampleinfo.innerHTML = "";
+  }else{
+    exampleinfo.innerHTML = info;
+  }
+
+  if(examplecontrols.innerHTML === undefined){
+    examplecontrols.innerHTML = "";
+  }else{
+    examplecontrols.innerHTML = controls;
+  }
+
+  clearbutton.style.visibility = "visible";
+  sourcebutton.style.visibility = "visible";
+
+  frame.src = link;
+  frame.focus();
+
+  sourcecode.href = source;
+
+  if(source === null){
+    sourcecode.removeAttribute("href");
+  }
+}
+
+function cleariframe(element){
+  var frame = document.getElementById("frame");
+  var sourcebutton = document.getElementById("sourcecode");
+  var exampleinfo = document.getElementById("exampleinfo");
+  var examplecontrols = document.getElementById("examplecontrols");
+
+  exampleinfo.innerHTML = "";
+  examplecontrols.innerHTML = "";
+
+  frame.removeAttribute("src");
+  element.style.visibility = "hidden";
+  sourcebutton.style.visibility = "hidden";
+}
---
+++ _site/json.js
@@ -0,0 +1,50 @@
+var JSONObject = {
+	"snow":{
+		"src":"http://www.whaleblubber.net/Kevin/html/snow/SnowFall.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/SnowFall",
+		"info":"Snow is a part of a weather collection I am making. Just a simple example to simulate snow.",
+		"input":"Move: A & D; 	 Jump: Space;"
+	},
+	"leaf":{
+		"src":"http://www.whaleblubber.net/Kevin/html/leaf/LeafFall.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/LeafFall",
+		"info":"Leaf is a part of a weather collection I am making. It is my take on how leaves fall. ",
+		"input":"",
+	},
+	"rain":{
+		"src":"http://www.whaleblubber.net/Kevin/html/rain/RainFall.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/RainFall",
+		"info":"Rain is a part of a weather collection I am making. I want to rework this one. It is hard to see.",
+		"input":"Move: A & D; 	 Jump: Space;",
+	},
+	"paulbunyan":{
+		"src":"http://www.whaleblubber.net/Kevin/html/paulbunyan/",
+		"code":"https://github.com/Levo/Pixi-Js/tree/master/Paul",
+		"info":"This game was created for Public Domain Jam // 2014. It was one week of development.",
+		"input":"Move: A & D & S & W; 	 Fire: LeftMouse;",
+	},
+	"coloredsquares":{
+		"src":"http://www.whaleblubber.net/Kevin/html/coloredsquares/ColorSquare.html",
+		"code":"https://github.com/Levo/ColoringSquares",
+		"info":"A program that is great for making truchet tiles.",
+		"input":"Small Triangle: 1 & 2 & 3 & 4;      Big Triangle: Q & W & E & R; 	 Fill: 5;",
+	},
+	"bleuetrouge":{
+		"src":"http://www.whaleblubber.net/Kevin/html/bleuetrouge",
+		"code":"",
+		"info":"I created this game for Clay.io's Got Game? competition // 2014. It was one month of development and a two week judging process. I placed 4th and won a Nexus 7.",
+		"input":"",
+	},
+	"bullethell":{
+		"src":"http://www.whaleblubber.net/Kevin/html/bullethell/BulletHell.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/BulletHell",
+		"info":"My first unfinished game.",
+		"input":"Move: Mouse;	  Fire: W;",
+	},
+	"rundodge":{
+		"src":"http://www.whaleblubber.net/Kevin/html/rundodge/SpeedRun.html",
+		"code":"https://github.com/Levo/DisplayMyExamples/tree/master/Threejs",
+		"info":"My first game with WebGL/Threejs that is unfinished.",
+		"input":"Move: A & D;	  Jump: Space;",
+	}
+};
