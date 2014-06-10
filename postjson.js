296eb8f6dc4154c5d7fc8ec696d1d26b81cb3daf
Kevin <khomer8@yahoo.com>
2014-06-09 21:28:13 -0500

    Added a new blog post
    -Working on a new blog post that shows off what it is capable of doing
    and what it lacks

--- postjson.js
+++ postjson.js
@@ -3,4 +3,8 @@ var JSONObject2 = {
 		"src":"posts/randomdungeon.html",
 		"code":"https://github.com/Levo/levo.github.io/blob/master/posts/randomdungeon.html",
 	},
+	"dungeontypes":{
+		"src":"posts/dungeontypes.html",
+		"code":"",
+	}
 };
