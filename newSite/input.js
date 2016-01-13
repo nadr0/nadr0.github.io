3e03516001c42cbf87c6d3a0ebd6e45183ed4a02
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-01-13 14:19:29 -0600

    starting to style & more functionality

--- newSite/input.js
+++ newSite/input.js
@@ -30,32 +30,56 @@ function loadData(data){
   /* Github repo button */
   var githubRepo = document.getElementById("githublinkButton");
 
+  var githubRepoImg = document.getElementById("githublinkImg");
 
   /* Set project text */
   projectText.innerHTML = data.info;
 
-  /* Set Github repo*/
-  githubRepo.href = data.code;
-  console.log(githubRepo);
-  console.log(githubRepo.href);
+  /* If there is a link to the github repo*/
+  if(data.code != ""){
+    /* Set Github repo*/
+    githubRepo.href = data.code;
+    /* Turn the repo link back on */
+    githubRepoImg.src = "images/GitHub-Mark-32px.png";
+  }else{
+    /* Turn off the github image for they don't get a link to a repo*/
+    githubRepoImg.src = "";
+  }
 
+  displayButtons();
+  displayProjectText();
 }
 
+/* 
+  Clear the current project info
+*/
+function clearData(){
+  hideProjectText();
+  hideButtons();
+  hide_iframe();
+}
+
+/* 
+  Display project text
+*/
+function displayProjectText(){
+  var projectText = document.getElementById("projectText");
+  projectText.style.display = "inline-block";
+} 
+
+/*
+  Hide project text
+*/
+function hideProjectText(){
+  var projectText = document.getElementById("projectText");
+  projectText.style.display = "none";
+}
 
 /* 
   Display buttons
 */
 
 function displayButtons(){
-
-  /* canvas button */
-  var canvasButton = document.getElementById("canvasButton");
-
-  /* github button*/
-  var githubButton = document.getElementById("codeButton");
-
-  /* clear button */
-  var clearButton = document.getElementById("clearcodeButton");
 
   /* Display the buttons*/
   canvasButton.style.display = "inline-block";
@@ -70,18 +94,28 @@ function displayButtons(){
 */
 
 function hideButtons(){
-    /* canvas button */
-  var canvasButton = document.getElementById("canvasButton");
-
-  /* github button*/
-  var githubButton = document.getElementById("codeButton");
-
-  /* clear button */
-  var clearButton = document.getElementById("clearcodeButton");
 
   /* Hide the buttons*/
   canvasButton.style.display = "none";
   githubButton.style.display = "none";
   clearButton.style.display = "none";
 
+}
+
+/*
+  Displays and populates the iframe element
+*/
+function display_iframe(data){
+  var iframe = document.getElementById("project_iframe");
+  iframe.style.display = "inline";
+  iframe.src = data.src;
+}
+
+/*
+  Clears and hides the iframe element
+*/
+function hide_iframe(){  
+  var iframe = document.getElementById("project_iframe");
+  iframe.style.display = "none";
+  iframe.src = "";
 }
