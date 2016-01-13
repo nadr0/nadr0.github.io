16957700e15706817fa4166b2c4c8fa0b107f525
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-01-13 13:37:09 -0600

    getting the canvas in place

--- newSite/input.js
+++ newSite/input.js
@@ -28,7 +28,60 @@ function loadData(data){
   var projectText = document.getElementById("projectText");
 
   /* Github repo button */
-  var githubRepo = document.getElementById("githublink");
+  var githubRepo = document.getElementById("githublinkButton");
 
+
+  /* Set project text */
+  projectText.innerHTML = data.info;
+
+  /* Set Github repo*/
+  githubRepo.href = data.code;
+  console.log(githubRepo);
+  console.log(githubRepo.href);
+
+}
+
+
+/* 
+  Display buttons
+*/
+
+function displayButtons(){
+
+  /* canvas button */
+  var canvasButton = document.getElementById("canvasButton");
+
+  /* github button*/
+  var githubButton = document.getElementById("codeButton");
+
+  /* clear button */
+  var clearButton = document.getElementById("clearcodeButton");
+
+  /* Display the buttons*/
+  canvasButton.style.display = "inline-block";
+  githubButton.style.display = "inline-block";
+  clearButton.style.display = "inline-block";
+
+}
+
+
+/* 
+  Hide buttons
+*/
+
+function hideButtons(){
+    /* canvas button */
+  var canvasButton = document.getElementById("canvasButton");
+
+  /* github button*/
+  var githubButton = document.getElementById("codeButton");
+
+  /* clear button */
+  var clearButton = document.getElementById("clearcodeButton");
+
+  /* Hide the buttons*/
+  canvasButton.style.display = "none";
+  githubButton.style.display = "none";
+  clearButton.style.display = "none";
 
 }
