349c036ac06bdbba9989573fa6b5ac6caf87793f
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-08-14 06:15:56 -0700

    updating my new site

---
+++ newSite/data.js
@@ -0,0 +1,203 @@
+var siteData = {
+    "project-paul":{
+        "src":"http://www.whaleblubber.net/Kevin/html/paulbunyan/",
+        "code":"https://github.com/Levo/Pixi-Js/tree/master/Paul",
+        "description":"The game was created for Public Doman Jam // 2014. It was one week of development.",
+        "controls":{
+            "movement":"WASD",
+            "fire":"left mouse",
+        },
+        "links":{
+            "itch.io":"http://itch.io/jam/the-public-domain-jam"
+        }
+    },
+    "project-bleuetrouge":{
+        "src":"http://www.whaleblubber.net/Kevin/html/bleuetrouge",
+        "code":"",
+        "description":"I created this game for Clay.io's Got Game? competition // 2014. It was one month of development and a two week judging process. I placed 4th and won a Nexus 7.",
+        "controls":{
+
+        },
+        "links":{
+            "waubonsee article":"http://www.waubonsee.edu/news/archive/2014/20140620VideoGameDesignContestNadro.php",
+            "winners":"http://clay.io/blog/announcing-the-got-game-2014-winners/",
+            "competition details":"ttp://clay.io/gotgame",
+        }
+    },
+    "project-random-dungeon":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-astar":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-gameboy":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-raycast":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-maze":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-graph":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-dither":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-lsystem":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-bezier":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-3dlsystem":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-cs445":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-btree":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-receipt":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-raytracer":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-algorithm":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+    "project-grabcut":{
+        "src":"",
+        "code":"",
+        "description":"",
+        "controls":{
+
+        },
+        "links":{
+
+        }
+    },
+}
---
+++ newSite/util.js
@@ -0,0 +1,84 @@
+function clearProject(){
+    var iframe = document.getElementById('iframe');
+    var code = document.getElementById('code');
+    var controls = document.getElementById('controls');
+    var links = document.getElementById('links');
+    var description = document.getElementById('description');
+
+    iframe.src = "";
+    code.href = "";
+    description.innerHTML = "";
+
+    while(controls.childNodes.length){
+        controls.removeChild(controls.firstChild);
+    }
+
+    while(links.childNodes.length){
+        links.removeChild(links.firstChild);
+    }
+
+}
+
+function populateProject(projectName){
+    clearProject();
+
+    var projectData = siteData[projectName];
+    var controls = document.getElementById('controls');
+    var links = document.getElementById('links');
+    var description = document.getElementById('description');
+
+    updateIframe(projectData.src);
+    updateGithub(projectData.code);
+
+    // Set description
+    description.innerHTML = projectData.description;
+
+    // Set iframe
+    iframe.src = projectData.src;
+
+    // Populate links
+    for(link in projectData.links){
+        var linkElement = document.createElement('a');
+        linkElement.classList.add('overlay-rows');
+        linkElement.innerHTML = link;
+        linkElement.href = projectData.links[link];
+        links.appendChild(linkElement);
+    }
+
+    // Populate controls
+    for(control in projectData.controls){
+        var controlElement = document.createElement('div');
+        controlElement.classList.add('overlay-rows');
+        controlElement.innerHTML = control + " : " + projectData.controls[control];
+        controls.appendChild(controlElement);
+    }
+
+}
+
+function displayCheck(elem,data,onDisplay,type){
+
+    if(data === ""){
+        elem.style.display = 'none';
+    }else{
+        elem.style.display = type;
+        onDisplay();
+    }
+}
+
+function updateIframe(data){
+    var iframe = document.getElementById('iframe');
+
+    displayCheck(iframe, data,
+        function(){
+            iframe.src = data
+        }, 'block');
+}
+
+function updateGithub(data){
+    var code = document.getElementById('code');
+
+    displayCheck(code, data,
+        function(){
+            code.href = data
+        }, 'block');
+}
