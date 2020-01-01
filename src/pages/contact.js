0b0511eab1c761eb95d9a5ac43ea459d075cd1df
Kevin Nadro <khomer8@yahoo.com>
2020-01-01 14:41:24 -0600

    say hello instead of say hi

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => (
   <Layout>
     <SEO title="Contact" />
     <p>
-      Have any questions, comments, corrections, or just want to say hi? Send me an email at <a href="mailto:sayhello@kevinnadro.com">sayhello@kevinadro.com</a>
+      Have any questions, comments, corrections, or just want to say hello? Send me an email at <a href="mailto:sayhello@kevinnadro.com">sayhello@kevinadro.com</a>
     </p>
     <h3>Social sites</h3>
     <ul>
