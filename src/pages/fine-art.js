06136779be499e1bea16d9059eb205996cebf06b
Kevin Nadro <kevin.nadro@gmail.com>
2023-07-09 12:32:51 -0500

    cleaned up fineart page, added press released, hopefully removed purple background on mobile

--- src/pages/fine-art.js
+++ src/pages/fine-art.js
@@ -9,16 +9,18 @@ const AboutMe = () => {
     <h1>Fine Art Photography</h1>
 
     <h3>CHICAGO WORKS: A Captivating Art Exhibition by <a href="https://j2gallery.com">Jackson Junge Gallery</a></h3>
-    <div style={{textAlign:'center'}}>
-      <img src="/bearing-the-elements.jpg"/>
-      <p><i>Bearing the elements</i></p>
+    <div style={{textAlign:'center', width: '600px', margin:'auto'}}>
+      <img style={{marginBottom:'0px'}} src="/bearing-the-elements.jpg"/>
+      <div><i style={{fontSize: '15px'}}>Kevin Nadro, Bearing the elements, Photograph, 24X36 Framed & Matted</i></div>
     </div>
 
     <blockquote>Far from romanticizing the city, this exhibition seeks to unveil the raw grittiness of Chicago's lived experience while nurturing a profound admiration and appreciation for its familiar aspects. In his artwork titled "Bearing the Elements”, Kevin Nadro captures the chaos and anxiety of traversing the city amidst never-ending construction. He remarks, "As I walk around my neighborhood of Wicker Park the improvements made to the infrastructure put a strain on small sidewalks and streets. Where are you supposed to park several large trucks on a residential street? The noise, detours, and space are apparent to all". One might smile as they read this description, knowing full well the pain of trying to find parking while construction is underway. And should we even mention wintertime “dibs” law?</blockquote>
 
-    <p>The <a href="https://j2gallery.com/exhibitions/">exhibition</a> runs July 11th, 2023 – September 3rd, 2023</p>
+    <div>The <a href="https://j2gallery.com/exhibitions/">exhibition</a> runs July 11th, 2023 – September 3rd, 2023.</div>
+    <div><a href="https://j2gallery.com/store/special-exhibit/bearing-the-elements">Store page</a></div>
+    <div><a href="https://www.meer.com/en/74772-chicago-works">Press release</a></div>
 
-    <a href="https://j2gallery.com/store/special-exhibit/bearing-the-elements">Store : Special Exhibit : BEARING THE ELEMENTS</a>
+
 
   </Layout>
   )
