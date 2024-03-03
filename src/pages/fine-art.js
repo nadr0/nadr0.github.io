415f28e8f2055c53f1d0a18e38052ca962aa1847
node-mail-myself <144860136+node-mail-myself@users.noreply.github.com>
2024-03-02 18:20:56 -0600

    Adding latest artwork

--- src/pages/fine-art.js
+++ src/pages/fine-art.js
@@ -8,6 +8,37 @@ const AboutMe = () => {
     <SEO title="Fine Art" />
     <h1>Fine Art Photography</h1>
 
+    <h3> No Color: Group Exhibition by <a href="https://j2gallery.com">Jackson Junge Gallery</a></h3>
+
+    <div style={{textAlign:'center'}}>
+      <img style={{marginBottom:'0px', width: '600px'}} src="/Still-Open.jpg"/>
+      <div><i style={{fontSize: '15px'}}>Kevin Nadro, Still Open, Photograph 24 ½" x 18 ½" (framed)</i></div>
+    </div>
+
+    <blockquote>Showcasing the talent of 33 artists, many of whom are local to the Chicagoland area, NO COLOR challenges conventional notions of artistic expression by exploring the limitless possibilities that emerge when black and white intersect. By removing color from the equation, artists are compelled to explore alternative avenues of communication and expression, leading to unexpected and compelling outcomes.</blockquote>
+    
+    <div><a href="https://j2gallery.com/store/special-exhibit/still-open">Still Open</a></div>
+    <div>The <a href="https://j2gallery.com/exhibitions/past">exhibition</a> runs February 26th, 2024 – April 7th 3rd, 2024.</div>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <h3> TEN by TEN: Showcasing Chicago’s Diverse Artistry by <a href="https://j2gallery.com">Jackson Junge Gallery</a></h3>
+
+    <div style={{textAlign:'center'}}>
+      <img style={{marginBottom:'0px', width: '600px'}} src="/Nadro_Koi_Koi-INSTA.jpg"/>
+      <div><i style={{fontSize: '15px'}}>Kevin Nadro, Koi Koi, Photograph, 10" x 10" Silver Halide Photograph on Aluminum</i></div>
+    </div>
+
+    <blockquote>Jackson Junge Gallery is thrilled to announce the highly anticipated return of its renowned TEN by TEN exhibition for the second consecutive year. This open-submission extravaganza proudly features over 300 talented artists from the heart of Chicago, with an impressive display of more than 400 captivating works of art. Commencing on November 3, 2023, and running until January 7, 2024, this exceptional showcase is open to the public, with no admission fee required.</blockquote>
+
+    <div><a href="    https://j2gallery.com/store/special-exhibit/koi-koi">Koi Koi</a></div>
+    <div>The <a href="https://j2gallery.com/exhibitions/past">exhibition</a> runs November 3rd, 2023 – January 7th, 2024.</div>
+
+    <br></br>
+    <br></br>
+    <br></br>
+
     <h3>CHICAGO WORKS: A Captivating Art Exhibition by <a href="https://j2gallery.com">Jackson Junge Gallery</a></h3>
     <div style={{textAlign:'center'}}>
       <img style={{marginBottom:'0px'}} src="/bearing-the-elements.jpg"/>
@@ -22,13 +53,12 @@ const AboutMe = () => {
       <div><i style={{fontSize: '15px'}}>Kevin Nadro, Strolls and Holes, Photograph, 37 ¼" x 25 ¼" (Framed & Matted)</i></div>
     </div>
 
-    <div><a href="https://j2gallery.com/store/special-exhibit/strolls-and-holes">Store page - strolls and holes</a></div>
-    <div><a href="https://j2gallery.com/store/special-exhibit/bearing-the-elements">Store page - bearing the elements</a></div>
+    <div><a href="https://j2gallery.com/store/special-exhibit/strolls-and-holes">strolls and holes</a></div>
+    <div><a href="https://j2gallery.com/store/special-exhibit/bearing-the-elements">bearing the elements</a></div>
 
 
-    <div>The <a href="https://j2gallery.com/exhibitions/">exhibition</a> runs July 11th, 2023 – September 3rd, 2023.</div>
+    <div>The <a href="https://j2gallery.com/exhibitions/past">exhibition</a> runs July 11th, 2023 – September 3rd, 2023.</div>
     <div><a href="https://www.meer.com/en/74772-chicago-works">Press release</a></div>
-
   </Layout>
   )
 }
