2ccdd54d53340a0212cd3ef73915fa4da81ef1bd
node-mail-myself <144860136+node-mail-myself@users.noreply.github.com>
2024-08-25 11:06:25 -0500

    Updates

---
+++ component---src-pages-404-js-8d28d460ffc2cae23cf1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),u=a("Bl7J"),o=a("vrFN");t.default=()=>l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist..."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-8d28d460ffc2cae23cf1.js.map
